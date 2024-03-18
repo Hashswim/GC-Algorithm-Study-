025f9f1b5f31fdc763f7f5a8f51517f868ed17d6
Aaron <57447946+Hashswim@users.noreply.github.com>
2024-03-18 09:45:23 +0900

    Create 서수영.swift

---
+++ 프로그래머스/체육복/서수영.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
+    var all = [Int](repeating: 1, count: n+2)
+    for l in lost{
+        all[l] -= 1
+    }
+    for r in reserve{
+        all[r] += 1
+    }
+    
+    for i in 1...n{
+
+        if all[i] == 0{
+            if all[i-1] == 2{
+                all[i-1] = 1
+                all[i] = 1
+            }else if all[i+1] == 2{
+                all[i+1] = 1
+                all[i] = 1
+            }
+        }
+    }        
+    return all.filter { $0>=1 }.count - 2
+}
